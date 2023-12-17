2ac1fb6e71426f972b61e34651bc2407a8c257f4
Yaroslav <6715414@gmail.com>
2023-12-17 14:42:34 +0200

    refactor: add page wrapper and loader

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,19 +1,20 @@
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
 import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader/ui/PageLoader';
 
-const AppRouter = () => {
-  return (
-    <Suspense fallback={<div>Loading</div>}>
-      <Routes>
-        {Object.values(routeConfig).map(({ element, path }) => (
-          <Route key={path} path={path} element={element} />
-        ))}
-      </Routes>
-    </Suspense>
-  );
-};
+const AppRouter = () => (
+  <Suspense fallback={<PageLoader />}>
+    <Routes>
+      {Object.values(routeConfig).map(({ element, path }) => (
+        <Route
+          key={path}
+          path={path}
+          element={<div className="page-wrapper">{element}</div>}
+        />
+      ))}
+    </Routes>
+  </Suspense>
+);
 
 export { AppRouter };
