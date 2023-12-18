3d9e6b68a1dbc3dcce7f1ab1dd1feb95ca26681e
Yaroslav <6715414@gmail.com>
2023-12-18 11:04:09 +0200

    feat: add PageError

---
+++ src/widgets/PageError/ui/PageError.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Button } from 'shared/ui/Button/Button';
+
+import cls from './PageError.module.scss';
+
+interface PageErrorProps {
+  className?: string
+
+}
+
+export const PageError: FC<PageErrorProps> = ({ className }) => {
+  const { t } = useTranslation();
+
+  const reloadPage = () => {
+    window.location.reload();
+  };
+
+  return (
+    <div className={classNames(cls.PageError, {}, [className])}>
+      <p>{t('error.message')}</p>
+      <Button onClick={reloadPage}>
+        {t('error.button')}
+      </Button>
+    </div>
+  );
+};
