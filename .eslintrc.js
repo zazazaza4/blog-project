95d41b3fb15ec3fe4d2d60280f521f2a7bdc2a1d
Yaroslav <6715414@gmail.com>
2023-12-30 12:22:48 +0200

    refactor: add new setup for linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     jest: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'plugin:i18next/recommended'],
+  extends: ['plugin:react/recommended', 'airbnb', 'plugin:i18next/recommended', 'plugin:storybook/recommended', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
@@ -16,7 +16,7 @@ module.exports = {
   plugins: ['react', '@typescript-eslint', 'simple-import-sort', 'i18next'],
   rules: {
     '@typescript-eslint/no-unused-vars': 'off',
-    'i18next/no-literal-string': ['error', { markupOnly: true }],
+    'i18next/no-literal-string': ['error', { markupOnly: true, ignoreAttribute: ['data-testid', 'to'] }],
     'import/extensions': 'off',
     'import/no-extraneous-dependencies': 'off',
     'import/no-unresolved': 'off',
@@ -24,6 +24,7 @@ module.exports = {
     'import/prefer-default-export': 'off',
     'no-shadow': 'off',
     'no-underscore-dangle': 'off',
+    'linebreak-style': 0,
     'react/function-component-definition': 'off',
     'react/jsx-filename-extension': [
       2,
@@ -52,4 +53,10 @@ module.exports = {
   globals: {
     __IS_DEV__: true,
   },
+  overrides: [{
+    files: ['**/src/**/*.test.{ts,tsx}'],
+    rules: {
+      'i18next/no-literal-string': 'off',
+    },
+  }],
 };
