310a24bcbcc321e51709543bd66071b6df90221a
Yaroslav <6715414@gmail.com>
2023-12-30 12:26:31 +0200

    refactor: update webpack loaders configuration

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,6 @@
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
 
+import { buildCssLoader } from './loaders/buildCssLoader';
 import { BuildOptions } from './types/config';
 
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
@@ -38,24 +38,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     },
   };
 
-  const cssLoader: webpack.RuleSetRule = {
-    test: /\.s[ac]ss$/i,
-    use: [
-      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-      {
-        loader: 'css-loader',
-        options: {
-          modules: {
-            auto: (resPath: string) => Boolean(resPath.includes('.module.')),
-            localIdentName: isDev
-              ? '[path][name]__[local]--[hash:base64:5]'
-              : '[hash:base64:7]',
-          },
-        },
-      },
-      'sass-loader',
-    ],
-  };
+  const cssLoader: webpack.RuleSetRule = buildCssLoader(isDev);
 
   const typescriptLoader: webpack.RuleSetRule = {
     test: /\.tsx?$/,
