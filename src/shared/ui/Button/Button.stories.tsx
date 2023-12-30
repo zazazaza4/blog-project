7f9817ef5729f22b8f6acefb7ffe67bfdccc1c39
Yaroslav <6715414@gmail.com>
2023-12-30 12:55:07 +0200

    test: add storybook for Button

---
+++ src/shared/ui/Button/Button.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Button, ThemeButton } from './Button';
+
+const meta = {
+  title: 'shared/Button',
+  component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    children: 'Test',
+  },
+};
+
+export const Clear: Story = {
+  args: {
+    children: 'Test',
+    theme: ThemeButton.CLEAR
+  },
+};
+
+export const Outline: Story = {
+  args: {
+    children: 'Test',
+    theme: ThemeButton.OUTLINE
+  },
+};
+
