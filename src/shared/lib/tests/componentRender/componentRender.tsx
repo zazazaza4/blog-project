6d9866e3ffea6ee4507f4c91a7a1c5240a9ac3db
Yaroslav <6715414@gmail.com>
2024-01-04 23:49:52 +0200

    feat: add redux to testing config

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -1,22 +1,29 @@
 import { ReactNode } from 'react';
 import { I18nextProvider } from 'react-i18next';
 import { MemoryRouter } from 'react-router-dom';
+import { DeepPartial } from '@reduxjs/toolkit';
 import { render } from '@testing-library/react';
+
+import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
 
 import i18nForTest from 'shared/config/i18n/i18nForTest';
 
 export interface ComponentRenderOptions {
   route?: string;
+  initialState?: DeepPartial<StateSchema>
 }
 
 export const componentRender = (component: ReactNode, options: ComponentRenderOptions = {}) => {
-  const { route = '/' } = options;
+  const { route = '/', initialState } = options;
 
   return render(
-    <MemoryRouter initialEntries={[route]}>
-      <I18nextProvider i18n={i18nForTest}>
-        {component}
-      </I18nextProvider>
-    </MemoryRouter>,
+    <StoreProvider initialState={initialState}>
+      <MemoryRouter initialEntries={[route]}>
+        <I18nextProvider i18n={i18nForTest}>
+          {component}
+        </I18nextProvider>
+      </MemoryRouter>
+    </StoreProvider>
+    ,
   );
 };
