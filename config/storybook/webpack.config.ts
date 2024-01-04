2ca3237983d634bc2a60d65fb139fd43760ccc8f
Yaroslav <6715414@gmail.com>
2024-01-04 19:29:58 +0200

    refactor: enhance webpack configuration for storybook

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -1,5 +1,5 @@
 import path from 'path';
-import webpack from 'webpack';
+import webpack, { RuleSetRule } from 'webpack';
 
 import { buildCssLoader } from '../build/loaders/buildCssLoader';
 import { BuildPaths } from '../build/types/config';
@@ -14,6 +14,21 @@ export default ({ config }: { config: webpack.Configuration }) => {
 
   config.resolve?.modules?.push(paths.src);
   config.resolve?.extensions?.push('.ts', '.tsx');
+
+  // eslint-disable-next-line no-param-reassign
+  // @ts-ignore
+  // eslint-disable-next-line no-param-reassign
+  config.module!.rules = config.module?.rules?.map((rule: RuleSetRule) => {
+    if (/svg/.test(rule.test as string)) {
+      return { ...rule, exclude: /\.svg$/i };
+    }
+    return rule;
+  });
+
+  config.module?.rules?.push({
+    test: /\.svg$/,
+    use: ['@svgr/webpack'],
+  });
   config.module?.rules?.push(buildCssLoader(true));
 
   return config;
