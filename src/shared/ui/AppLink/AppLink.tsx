1963f17df3309d7735897f1dec186515437a1e4f
Yaroslav <6715414@gmail.com>
2024-01-04 18:58:43 +0200

    refactor: update AppLink component with new theme options

--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -1,12 +1,14 @@
 import { FC } from 'react';
 import { Link, LinkProps } from 'react-router-dom';
+
 import { classNames } from 'shared/lib/classNames/classNames';
 
 import cls from './AppLink.module.scss';
 
 export enum AppLinkTheme {
   PRIMARY = 'primary',
-  SECONDARY = 'secondary',
+  INVERTED = 'inverted',
+  RED = 'red'
 }
 
 interface AppLinkProps extends LinkProps {
