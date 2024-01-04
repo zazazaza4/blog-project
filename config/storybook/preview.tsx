d6eee96f2fb638cc25c5a462e112cebacb58e44b
Yaroslav <6715414@gmail.com>
2024-01-04 18:55:17 +0200

    feat: add decorators for storybook

---
+++ config/storybook/preview.tsx
@@ -0,0 +1,25 @@
+import type { Preview } from '@storybook/react';
+
+import { Theme } from '../../src/app/providers/ThemeProvider';
+import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
+import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
+import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+
+const preview: Preview = {
+  decorators: [
+    StyleDecorator,
+    ThemeDecorator(Theme.LIGHT),
+    RouterDecorator,
+  ],
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
