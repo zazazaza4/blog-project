c12ca1023f7459a401fe0df44699395bbeabfc89
Yaroslav <6715414@gmail.com>
2024-01-04 19:33:47 +0200

    feat: update Navbar component

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,7 +1,10 @@
-import { FC } from 'react';
+/* eslint-disable i18next/no-literal-string */
+import { FC, useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+
 import { classNames } from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { Modal } from 'shared/ui/Modal/Modal';
 
 import cls from './Navbar.module.scss';
 
@@ -11,17 +14,19 @@ interface NavbarProps {
 
 export const Navbar: FC<NavbarProps> = ({ className }) => {
   const { t } = useTranslation();
+  const [isAuthModal, setIsAuthModal] = useState(false);
+
+  const onToggleModal = useCallback(() => {
+    setIsAuthModal((prev) => !prev);
+  }, []);
 
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
-      <div className={cls.links}>
-        <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
-          {t('header.main')}
-        </AppLink>
-        <AppLink theme={AppLinkTheme.SECONDARY} to="/about">
-          {t('header.about')}
-        </AppLink>
-      </div>
+      <Button theme={ButtonTheme.CLEAR_INVERTED} onClick={onToggleModal} className={cls.links}>{t('header.login')}</Button>
+      <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Explicabo qui voluptates libero quo, odit itaque deleniti praesentium dolorum amet fugit.
+      </Modal>
     </div>
   );
 };
