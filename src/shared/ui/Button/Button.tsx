56fe40d6d78516485936ddce86538fa85ea9ff43
Yaroslav <6715414@gmail.com>
2024-01-04 19:28:20 +0200

    refactor: update Button component with ButtonTheme, ButtonSize, and isSquare option

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,29 +1,49 @@
 import { ButtonHTMLAttributes, FC } from 'react';
+
 import { classNames } from 'shared/lib/classNames/classNames';
 
 import cls from './Button.module.scss';
 
-export enum ThemeButton {
+export enum ButtonTheme {
   CLEAR = 'clear',
-  OUTLINE = 'outline'
+  CLEAR_INVERTED = 'clearInverted',
+  OUTLINE = 'outline',
+  BACKGROUND = 'background',
+  BACKGROUND_INVERTED = 'backgroundInverted',
+}
+
+export enum ButtonSize {
+  M = 'size_m',
+  L = 'size_l',
+  XL = 'size_xl',
 }
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
-  theme?: ThemeButton;
+  theme?: ButtonTheme;
+  size?: ButtonSize;
+  isSquare?: boolean;
 }
 
 export const Button: FC<ButtonProps> = ({
   className,
   children,
   theme,
+  isSquare,
+  size = ButtonSize.M,
   ...otherProps
-}) => (
-  <button
-    type="button"
-    className={classNames(cls.Button, {}, [className, cls[theme]])}
-    {...otherProps}
-  >
-    {children}
-  </button>
-);
+}) => {
+  const mods: Record<string, boolean> = {
+    [cls.square]: isSquare,
+  };
+
+  return (
+    <button
+      type="button"
+      className={classNames(cls.Button, mods, [className, cls[theme], cls[size]])}
+      {...otherProps}
+    >
+      {children}
+    </button>
+  );
+};
