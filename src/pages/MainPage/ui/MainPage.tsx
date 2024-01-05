dc49c44c776c750c67b46ab02193efb2528bacf0
Yaroslav <6715414@gmail.com>
2024-01-05 14:29:55 +0200

    chore: remove unnecessary imports and components from MainPage

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,18 +1,11 @@
 import { useTranslation } from 'react-i18next';
 
-import { BugButton } from 'app/providers/ErrorBoundary';
-
-import { Counter } from 'entities/Counter';
-
 const MainPage = () => {
   const { t } = useTranslation('main');
 
   return (
     <div>
-      <BugButton />
-      {' '}
       {t('title')}
-      <Counter />
     </div>
   );
 };
