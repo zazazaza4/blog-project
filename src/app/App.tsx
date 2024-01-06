14cc6f08e4bd370e92f8730e1e9815b0beb91c11
Yaroslav <6715414@gmail.com>
2024-01-06 12:55:45 +0200

    refactor: enhance initialization of user authentication data

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,32 @@
-import { Suspense, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
+
+import { userActions } from 'entities/User';
 
 import { classNames } from 'shared/lib/classNames/classNames';
 
 import { AppRouter } from './providers/router';
 
-const App = () => (
-  <div className={classNames('app', {}, [])}>
-    <Suspense fallback="">
-      <Navbar />
-      <div className="content-page">
-        <Sidebar />
-        <AppRouter />
-      </div>
-    </Suspense>
-  </div>
-);
+const App = () => {
+  const dispath = useDispatch();
+
+  useEffect(() => {
+    dispath(userActions.initAuthData());
+  }, [dispath]);
+
+  return (
+    <div className={classNames('app', {}, [])}>
+      <Suspense fallback="">
+        <Navbar />
+        <div className="content-page">
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
+    </div>
+  );
+};
 export default App;
