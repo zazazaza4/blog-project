9f53cf5e3b3e3fe7cb08ac9c86426a4854b4acb2
Yaroslav <6715414@gmail.com>
2024-01-06 15:03:46 +0200

    feat: implement DynamicModuleLoader component for dynamic Redux reducer loading and unloading

---
+++ src/shared/lib/components/DynamicModuleLoader/DynamicModuleLoader.tsx
@@ -0,0 +1,45 @@
+import { FC, ReactElement, useEffect } from 'react';
+import { useDispatch, useStore } from 'react-redux';
+import { Reducer } from '@reduxjs/toolkit';
+
+import { ReduxStoreWithManager } from 'app/providers/StoreProvider';
+import { StateSchemaKey } from 'app/providers/StoreProvider/config/StateSchema';
+
+export type ReducersList = {
+  [name in StateSchemaKey]?: Reducer;
+}
+
+type ReducersListEntry = [StateSchemaKey, Reducer]
+
+interface DynamicModuleLoaderProps {
+  children: ReactElement;
+  reducers: ReducersList;
+  removeAfterUnmount?: boolean;
+}
+
+export const DynamicModuleLoader: FC<DynamicModuleLoaderProps> = ({
+  children, reducers, removeAfterUnmount,
+}) => {
+  const dispatch = useDispatch();
+  const store = useStore() as ReduxStoreWithManager;
+
+  useEffect(() => {
+    Object.entries(reducers).forEach(([key, reducer]: ReducersListEntry) => {
+      store.reducerManger.add(key, reducer);
+      dispatch({ type: `@INIT ${key} reducer` });
+    });
+
+    return () => {
+      if (removeAfterUnmount) {
+        Object.entries(reducers).forEach(([key]: ReducersListEntry) => {
+          store.reducerManger.remove(key);
+          dispatch({ type: `@DESTROY ${key} reducer` });
+        });
+      }
+    };
+
+    // eslint-disable-next-line
+    }, []);
+
+  return children;
+};
