0b3f4ba0a0f675f3dd81f310b7593b0494284607
Yaroslav <6715414@gmail.com>
2024-01-07 14:47:56 +0200

    feat: add useAppDispatch hook

--- src/app/providers/StoreProvider/index.ts
+++ src/app/providers/StoreProvider/index.ts
@@ -1,3 +1,3 @@
 export type { ReduxStoreWithManager, StateSchema } from './config/StateSchema';
-export { createReduxStore } from './config/store';
+export { AppDispatch, createReduxStore } from './config/store';
 export { StoreProvider } from './ui/StoreProvider';
