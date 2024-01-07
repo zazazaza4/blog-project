1e3dfe2e0f93631d0845cce104f23f3c4f87f237
Yaroslav <6715414@gmail.com>
2024-01-07 18:45:34 +0200

    feat: update ProfilePage with ProfileCard and data fetching

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,19 +1,33 @@
+import { FC, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
-import { profileReducer } from 'entities/Profile';
+import { fetchProfileData, ProfileCard, profileReducer } from 'entities/Profile';
 
+import { classNames } from 'shared/lib/classNames/classNames';
 import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 
 const reducers: ReducersList = {
   profile: profileReducer,
 };
 
-const ProfilePage = () => {
+interface ProfilePageProps {
+  className?: string
+}
+
+const ProfilePage: FC<ProfilePageProps> = ({ className }) => {
   const { t } = useTranslation('profile');
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProfileData());
+  }, [dispatch]);
 
   return (
     <DynamicModuleLoader removeAfterUnmount reducers={reducers}>
-      {t('title')}
+      <div className={classNames('', {}, [className])}>
+        <ProfileCard />
+      </div>
     </DynamicModuleLoader>
   );
 };
