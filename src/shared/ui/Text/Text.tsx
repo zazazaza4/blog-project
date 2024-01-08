dbbbbc3ce147977107fbd17bc95ac1861f43b320
Yaroslav <6715414@gmail.com>
2024-01-08 11:09:15 +0200

    feat: enhance Text component with align prop

--- src/shared/ui/Text/Text.tsx
+++ src/shared/ui/Text/Text.tsx
@@ -9,20 +9,28 @@ export enum TextTheme {
     ERROR = 'error'
 }
 
+export enum TextAlign {
+  RIGHT = 'right',
+  LEFT = 'left',
+  CENTER = 'center'
+}
+
 interface TextProps {
   className?: string
   title?: string;
   text?: string;
   theme?: TextTheme;
+  align?: TextAlign;
 }
 
 export const Text: FC<TextProps> = memo(({
   className,
-  text,
-  title,
+  text = '',
+  title = '',
   theme = TextTheme.PRIMARY,
+  align = TextAlign.LEFT,
 }: TextProps) => (
-  <div className={classNames(cls.Text, {}, [className, cls[theme]])}>
+  <div className={classNames(cls.Text, {}, [className, cls[theme], cls[align]])}>
     {title && <p className={cls.title}>{title}</p>}
     {text && <p className={cls.text}>{text}</p>}
   </div>
