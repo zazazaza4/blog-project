91f8678956fd7b9cd5cd88ea8a05682675ea1c29
Yaroslav <6715414@gmail.com>
2024-01-13 12:43:38 +0200

    chore: add react component template

---
+++ .fttemplates/React component/[FTName].stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Theme } from 'app/providers/ThemeProvider';
+
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+
+import { [FTName] } from './[FTName]';
+
+const meta = {
+  title: 'shared/[FTName]',
+  component: [FTName],
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof [FTName]>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Light: Story = {};
+
+export const Dark: Story = {};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
---
+++ .fttemplates/React component/[FTName].tsx
@@ -0,0 +1,24 @@
+import { FC, memo } from 'react'
+import { useTranslation } from 'react-i18next';
+
+import { classNames } from 'shared/lib/classNames/classNames';
+
+import cls from './[FTName].module.scss';
+
+interface [FTName]Props {
+  className?: string
+  
+}
+
+export const [FTName]: FC<[FTName]Props> = memo((
+  { 
+    className 
+  }: [FTName]Props) => {
+const { t } = useTranslation();
+
+  return (
+    <div className={classNames(cls.[FTName], {}, [className])}>
+     {t('')}
+    </div>
+  )
+});
