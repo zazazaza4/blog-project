f45a079d7992bf0dcc666e3fa5768406b95a9a45
Yaroslav <6715414@gmail.com>
2024-01-13 12:36:06 +0200

    feat: add Skeleton component

---
+++ src/shared/ui/Skeleton/Skeleton.stories.tsx
@@ -0,0 +1,49 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Theme } from 'app/providers/ThemeProvider';
+
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+
+import { Skeleton } from './Skeleton';
+
+const meta = {
+  title: 'shared/Skeleton',
+  component: Skeleton,
+  tags: ['autodocs'],
+  argTypes: {},
+} satisfies Meta<typeof Skeleton>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Light: Story = {
+  args: {
+    width: '100%',
+    height: '200px',
+  },
+};
+
+export const Circle: Story = {
+  args: {
+    border: '50%',
+    width: 100,
+    height: 100,
+  },
+};
+
+export const Dark: Story = {
+  args: {
+    width: '100%',
+    height: '200px',
+  },
+};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
+
+export const CircleDark: Story = {
+  args: {
+    border: '50%',
+    width: 100,
+    height: 100,
+  },
+};
+CircleDark.decorators = [ThemeDecorator(Theme.DARK)];
