ca18d8413057bfaac486b68b8c33b1269b9557e7
Yaroslav <6715414@gmail.com>
2024-01-16 14:21:21 +0200

    chore: update StoreDecorator to include articleDetailsReducer

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -4,6 +4,7 @@ import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
 
 import { loginReducer } from 'features/AuthByUsername/model/slice/loginSlice';
 
+import { articleDetailsReducer } from 'entities/Article/model/slice/articleDetailsSlice';
 import { profileReducer } from 'entities/Profile';
 
 import { ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
@@ -11,6 +12,7 @@ import { ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicM
 const defaultAsyncReducers: ReducersList = {
   loginForm: loginReducer,
   profile: profileReducer,
+  articleDetails: articleDetailsReducer,
 };
 
 export const StoreDecorator = (
