cba125a59339561ae6a89e9a53166c4be29e96bf
Yaroslav <6715414@gmail.com>
2024-01-16 14:40:46 +0200

    fix: import path for Country

--- src/entities/Country/ui/CountrySelect/CountrySelect.tsx
+++ src/entities/Country/ui/CountrySelect/CountrySelect.tsx
@@ -4,7 +4,7 @@ import { useTranslation } from 'react-i18next';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { Select } from 'shared/ui/Select/Select';
 
-import { Country } from '../../model/types/Country';
+import { Country } from '../../model/types/country';
 
 interface CountrySelectProps {
   className?: string;
