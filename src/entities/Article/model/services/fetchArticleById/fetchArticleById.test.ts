d66bfc8ab67c6e3784005f82b9f4c56ee8585c58
Yaroslav <6715414@gmail.com>
2024-01-16 14:20:01 +0200

    feat: create fetchArticleById Thunk for fetching article details by ID

---
+++ src/entities/Article/model/services/fetchArticleById/fetchArticleById.test.ts
@@ -0,0 +1,33 @@
+import { TestAsyncThunk } from 'shared/lib/tests/TestAsyncThunk/TestAsyncThunk';
+
+import { fetchArticleById } from './fetchArticleById';
+
+const data = {
+  id: '1',
+  title: '',
+};
+
+describe('fetchProfileData', () => {
+  test('success', async () => {
+    const thunk = new TestAsyncThunk(fetchArticleById);
+    thunk.api.get.mockReturnValue(Promise.resolve({
+      data,
+    }));
+    const result = await thunk.callThunk('1');
+
+    expect(thunk.api.get).toHaveBeenCalled();
+    expect(result.meta.requestStatus).toBe('fulfilled');
+    expect(result.payload).toEqual(data);
+  });
+
+  test('error', async () => {
+    const thunk = new TestAsyncThunk(fetchArticleById);
+    thunk.api.get.mockReturnValue(Promise.resolve({
+      status: 403,
+    }));
+    const result = await thunk.callThunk('1');
+
+    expect(thunk.api.get).toHaveBeenCalled();
+    expect(result.meta.requestStatus).toBe('rejected');
+  });
+});
