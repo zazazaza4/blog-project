222a58f6b7b3facf32e808f080588bf8065a95d4
Yaroslav <6715414@gmail.com>
2024-01-16 14:17:39 +0200

    feat: enhance ProfilePage with dynamic form and validation

--- src/pages/ProfilePage/ui/ProfilePage.stories.ts
+++ src/pages/ProfilePage/ui/ProfilePage.stories.ts
@@ -2,8 +2,13 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import { Theme } from 'app/providers/ThemeProvider';
 
+import { Country } from 'entities/Country';
+import { Currency } from 'entities/Currency';
+
 import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+
+import { AvatarImg } from 'shared/assets/tests/storybook.png';
 
 import ProfilePage from './ProfilePage';
 
@@ -18,7 +23,33 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Light: Story = {};
-Light.decorators = [StoreDecorator({})];
+Light.decorators = [StoreDecorator({
+  profile: {
+    form: {
+      username: '21212',
+      age: 32,
+      lastName: 'last name',
+      firstName: 'first name',
+      city: 'Kharkiv',
+      country: Country.Ukraine,
+      currency: Currency.UAH,
+      avatar: AvatarImg,
+    },
+  },
+})];
 
 export const Dark: Story = {};
-Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({})];
+Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({
+  profile: {
+    form: {
+      username: '21212',
+      age: 32,
+      lastName: 'last name',
+      firstName: 'first name',
+      city: 'Kharkiv',
+      country: Country.Ukraine,
+      currency: Currency.UAH,
+      avatar: AvatarImg,
+    },
+  },
+})];
--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,11 +1,30 @@
-import { FC, useEffect } from 'react';
+import {
+  FC, useCallback, useEffect, useMemo,
+} from 'react';
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 
-import { fetchProfileData, ProfileCard, profileReducer } from 'entities/Profile';
+import { Country } from 'entities/Country';
+import { Currency } from 'entities/Currency';
+import {
+  fetchProfileData,
+  getProfileError,
+  getProfileForm,
+  getProfileIsLoading,
+  getProfileReadonly,
+  getProfileValidateErrors,
+  profileActions,
+  ProfileCard,
+  profileReducer,
+  ValidateProfileError,
+} from 'entities/Profile';
 
 import { classNames } from 'shared/lib/classNames/classNames';
 import { DynamicModuleLoader, ReducersList } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
 import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { Text, TextTheme } from 'shared/ui/Text/Text';
+
+import { ProfilePageHeader } from './ProfilePageHeader/ProfilePageHeader';
 
 const reducers: ReducersList = {
   profile: profileReducer,
@@ -18,15 +37,114 @@ interface ProfilePageProps {
 const ProfilePage: FC<ProfilePageProps> = ({ className }) => {
   const { t } = useTranslation('profile');
   const dispatch = useAppDispatch();
+  const formData = useSelector(getProfileForm);
+  const error = useSelector(getProfileError);
+  const isLoading = useSelector(getProfileIsLoading);
+  const readonly = useSelector(getProfileReadonly);
+  const validateErrors = useSelector(getProfileValidateErrors);
+
+  const validateErrorTranslates = useMemo(
+    () => ({
+      [ValidateProfileError.SERVER_ERROR]: t('error.server'),
+      [ValidateProfileError.INCORRECT_AGE]: t('error.age'),
+      [ValidateProfileError.NO_DATA]: t('error.data'),
+      [ValidateProfileError.INCORRECT_USER_DATA]: t('error.user'),
+    }),
+    [t],
+  );
 
   useEffect(() => {
-    dispatch(fetchProfileData());
+    if (__PROJECT__ !== 'storybook') {
+      dispatch(fetchProfileData());
+    }
   }, [dispatch]);
+
+  const onChangeFirstname = useCallback(
+    (value: string = '') => {
+      dispatch(profileActions.updateProfile({ firstName: value }));
+    },
+    [dispatch],
+  );
+
+  const onChangeLastname = useCallback(
+    (value: string = '') => {
+      dispatch(profileActions.updateProfile({ firstName: value }));
+    },
+    [dispatch],
+  );
+
+  const onChangeCity = useCallback(
+    (value: string = '') => {
+      dispatch(profileActions.updateProfile({ city: value }));
+    },
+    [dispatch],
+  );
+
+  const onChangeAge = useCallback(
+    (value: string = '0') => {
+      const regex = /^[0-9\b]+$/;
+      if (value === '0' || regex.test(value)) {
+        dispatch(profileActions.updateProfile({ age: Number(value) }));
+      }
+    },
+    [dispatch],
+  );
+
+  const onChangeUsername = useCallback(
+    (value: string = '') => {
+      dispatch(profileActions.updateProfile({ username: value }));
+    },
+    [dispatch],
+  );
+
+  const onChangeAvatar = useCallback(
+    (value: string = '') => {
+      dispatch(profileActions.updateProfile({ avatar: value }));
+    },
+    [dispatch],
+  );
+
+  const onChangeCurrency = useCallback(
+    (currency: Currency = Currency.USD) => {
+      dispatch(profileActions.updateProfile({ currency }));
+    },
+    [dispatch],
+  );
+
+  const onChangeCountry = useCallback(
+    (country: Country = Country.Ukraine) => {
+      dispatch(profileActions.updateProfile({ country }));
+    },
+    [dispatch],
+  );
 
   return (
     <DynamicModuleLoader removeAfterUnmount reducers={reducers}>
       <div className={classNames('', {}, [className])}>
-        <ProfileCard />
+        <ProfilePageHeader />
+        {
+          validateErrors?.length && validateErrors.map((err) => (
+            <Text
+              key={err}
+              theme={TextTheme.ERROR}
+              text={validateErrorTranslates[err]}
+            />
+          ))
+        }
+        <ProfileCard
+          data={formData}
+          error={error}
+          readonly={readonly}
+          isLoading={isLoading}
+          onChangeFirstname={onChangeFirstname}
+          onChangeLastname={onChangeLastname}
+          onChangeAge={onChangeAge}
+          onChangeCity={onChangeCity}
+          onChangeAvatar={onChangeAvatar}
+          onChangeUsername={onChangeUsername}
+          onChangeCurrency={onChangeCurrency}
+          onChangeCountry={onChangeCountry}
+        />
       </div>
     </DynamicModuleLoader>
   );
