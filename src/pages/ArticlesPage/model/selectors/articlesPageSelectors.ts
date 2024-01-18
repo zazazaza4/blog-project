f9d00852ff8733f536c6ad88caf0328664763236
Yaroslav <6715414@gmail.com>
2024-01-18 19:13:11 +0200

    feat: add selectors for Article page

---
+++ src/pages/ArticlesPage/model/selectors/articlesPageSelectors.ts
@@ -0,0 +1,7 @@
+import { StateSchema } from 'app/providers/StoreProvider';
+
+import { ArticleView } from 'entities/Article';
+
+export const getArticlesPageView = (state: StateSchema) => state.articlesPage?.view || ArticleView.SMALL;
+export const getArticlesPageIsLoading = (state: StateSchema) => state.articlesPage?.isLoading || false;
+export const getArticlesPageError = (state: StateSchema) => state.articlesPage?.error || '';
