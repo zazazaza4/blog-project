d08c084728f2a6a698aa4ef9d06e52c9348076e7
Yaroslav <6715414@gmail.com>
2024-01-21 16:24:29 +0200

    feat: enhance Card component with theme support

--- src/shared/ui/Card/Card.tsx
+++ src/shared/ui/Card/Card.tsx
@@ -6,19 +6,30 @@ import { classNames } from 'shared/lib/classNames/classNames';
 
 import cls from './Card.module.scss';
 
+export enum CardTheme {
+  NORMAL = 'normal',
+  OUTLINE = 'outline'
+}
+
 interface CardProps extends HTMLAttributes<HTMLDivElement> {
   className?: string
   children: ReactNode;
+  theme?: CardTheme
 }
 
 export const Card: FC<CardProps> = (
-  {
+  props,
+) => {
+  const {
     className,
     children,
+    theme = CardTheme.NORMAL,
     ...otherProps
-  },
-) => (
-  <div className={classNames(cls.Card, {}, [className])} {...otherProps}>
-    {children}
-  </div>
-);
+  } = props;
+
+  return (
+    <div className={classNames(cls.Card, {}, [className, cls[theme]])} {...otherProps}>
+      {children}
+    </div>
+  );
+};
