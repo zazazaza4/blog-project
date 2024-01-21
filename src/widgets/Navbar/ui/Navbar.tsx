87a54f5fc880a910ba955cc84ba218b99cbeb112
Yaroslav <6715414@gmail.com>
2024-01-21 16:33:48 +0200

    refactor: redesign Navbar component with Text, AppLink, and Button

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -8,8 +8,11 @@ import { LoginModal } from 'features/AuthByUsername';
 
 import { getUserAuthData, userActions } from 'entities/User';
 
+import { RoutesPath } from 'shared/config/routeConfig/routeConfig';
 import { classNames } from 'shared/lib/classNames/classNames';
+import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { Text, TextTheme } from 'shared/ui/Text/Text';
 
 import cls from './Navbar.module.scss';
 
@@ -38,6 +41,18 @@ export const Navbar: FC<NavbarProps> = memo(({ className }: NavbarProps) => {
   if (authData) {
     return (
       <header className={classNames(cls.Navbar, {}, [className])}>
+        <Text
+          className={cls.appName}
+          title={t('Blog')}
+          theme={TextTheme.INVERTED}
+        />
+        <AppLink
+          to={RoutesPath.article_create}
+          theme={AppLinkTheme.INVERTED}
+          className={cls.createBtn}
+        >
+          {t('')}
+        </AppLink>
         <Button
           theme={ButtonTheme.CLEAR_INVERTED}
           onClick={onLogout}
