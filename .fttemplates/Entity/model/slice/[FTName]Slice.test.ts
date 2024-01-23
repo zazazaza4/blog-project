9572bcd163f705f68b6058fae0043b37eb3c22e5
Yaroslav <6715414@gmail.com>
2024-01-23 16:17:47 +0200

    chore: delete Entity template

--- .fttemplates/Entity/model/slice/[FTName]Slice.test.ts
+++
@@ -1,9 +0,0 @@
-import { [FTName]Schema } from '../types/[FTName]Schema';
-import { [FTName]Actions, [FTName]Reducer } from './[FTName]Slice';
-
-describe('[FTName]Slice', () => {
-  test('test', () => {
-    const state: DeepPartial<[FTName]Schema> = { };
-    expect([FTName]Reducer(state as [FTName]Schema, [FTName]Actions.setText('1'))).toEqual('1');
-  });
-});
--- .fttemplates/Entity/model/types/[FTName]Schema.ts
+++
@@ -1,4 +0,0 @@
-export interface[FTName]Schema {
-    isLoading: boolean;
-    error ?: string;
-}
