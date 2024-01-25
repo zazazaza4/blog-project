4302c5d81a6b1f2deb8dc286f8571526420802f9
Yaroslav <6715414@gmail.com>
2024-01-25 12:27:19 +0200

    feat: add new Notification entity for enhanced system notifications

---
+++ src/entities/Notification/ui/NotificationItem/NotificationItem.tsx
@@ -0,0 +1,44 @@
+import { FC, memo } from 'react';
+
+import { classNames } from 'shared/lib/classNames/classNames';
+import { AppLink } from 'shared/ui/AppLink/AppLink';
+import { Card, CardTheme } from 'shared/ui/Card/Card';
+import { Text } from 'shared/ui/Text/Text';
+
+import { Notification } from '../../model/types/notification';
+
+import cls from './NotificationItem.module.scss';
+
+interface NotificationItemProps {
+  className?: string;
+  item: Notification;
+}
+
+export const NotificationItem: FC<NotificationItemProps> = memo((
+  props: NotificationItemProps,
+) => {
+  const { className, item } = props;
+
+  const content = (
+    <Card
+      theme={CardTheme.OUTLINE}
+      className={classNames(cls.NotificationItem, {}, [className])}
+    >
+      <Text title={item.title} text={item.description} />
+    </Card>
+  );
+
+  if (item.href) {
+    return (
+      <AppLink
+        target="_blank"
+        className={cls.link}
+        to={item.href}
+      >
+        {content}
+      </AppLink>
+    );
+  }
+
+  return content;
+});
---
+++ src/entities/Notification/ui/NotificationList/NotificationList.tsx
@@ -0,0 +1,42 @@
+import { FC, memo } from 'react';
+
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Skeleton } from 'shared/ui/Skeleton/Skeleton';
+import { VStack } from 'shared/ui/Stack';
+
+import { useNotifications } from '../../api/notificationApi';
+import { NotificationItem } from '../NotificationItem/NotificationItem';
+
+import cls from './NotificationList.module.scss';
+
+interface NotificationListProps {
+  className?: string
+
+}
+
+export const NotificationList: FC<NotificationListProps> = memo((
+  props: NotificationListProps,
+) => {
+  const { className } = props;
+  const { data, isLoading } = useNotifications(null, {
+    pollingInterval: 10000,
+  });
+
+  if (isLoading) {
+    return (
+      <VStack max gap="16" className={classNames(cls.NotificationList, {}, [className])}>
+        <Skeleton width="100%" border="8px" height="80px" />
+        <Skeleton width="100%" border="8px" height="80px" />
+        <Skeleton width="100%" border="8px" height="80px" />
+      </VStack>
+    );
+  }
+
+  return (
+    <VStack max gap="16" className={classNames(cls.NotificationList, {}, [className])}>
+      {data?.map((item) => (
+        <NotificationItem item={item} key={item.id} />
+      ))}
+    </VStack>
+  );
+});
