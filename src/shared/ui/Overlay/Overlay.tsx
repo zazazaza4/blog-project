98475a91fd89b8b54537dab2d75e956ddfb98a1b
Yaroslav <6715414@gmail.com>
2024-01-25 14:05:32 +0200

    feat: add Overlay shared props

---
+++ src/shared/ui/Overlay/Overlay.tsx
@@ -0,0 +1,23 @@
+import { FC, memo } from 'react';
+
+import { classNames } from 'shared/lib/classNames/classNames';
+
+import cls from './Overlay.module.scss';
+
+interface OverlayProps {
+  className?: string;
+  onClick?: () => void;
+}
+
+export const Overlay: FC<OverlayProps> = memo((
+  props: OverlayProps,
+) => {
+  const {
+    className,
+    onClick,
+  } = props;
+
+  return (
+    <div onClick={onClick} className={classNames(cls.Overlay, {}, [className])} />
+  );
+});
