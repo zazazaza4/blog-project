2325d624ffdfe8b6886db6e29ce191754cafa864
Yaroslav <6715414@gmail.com>
2024-01-27 18:12:37 +0200

    fix: update path in templates

--- .fttemplates/React component/[FTName].tsx
+++ .fttemplates/React component/[FTName].tsx
@@ -1,25 +1,26 @@
 import { FC, memo } from 'react'
 import { useTranslation } from 'react-i18next';
 
-import { classNames } from 'shared/lib/classNames/classNames';
+import { classNames } from '@/shared/lib/classNames/classNames';
 
 import cls from './[FTName].module.scss';
 
-interface [FTName]Props {
-  className?: string
-  
+interface
+[FTName]Props {
+  className ?: string
+
 }
 
 export const [FTName]: FC<[FTName]Props> = memo((
   props: [FTName]Props) => {
   const { t } = useTranslation();
-  const { 
-    className 
+  const {
+    className
   } = props;
 
   return (
     <div className={classNames(cls.[FTName], {}, [className])}>
-     {t('')}
+      {t('')}
     </div>
   )
 });
