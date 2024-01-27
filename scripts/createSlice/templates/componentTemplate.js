d732b001c92caa94ca4ec7c5d07213141784d194
Yaroslav <6715414@gmail.com>
2024-01-27 18:15:33 +0200

    refactor: update paths and margins in slice template

--- scripts/createSlice/templates/componentTemplate.js
+++ scripts/createSlice/templates/componentTemplate.js
@@ -1,6 +1,6 @@
 const interfaceConst = 'interface';
 
-module.exports = (componentName) => `import { classNames } from 'shared/lib/classNames/classNames';
+module.exports = (componentName) => `import { classNames } from '@/shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import cls from './${componentName}.module.scss';
 import { memo } from 'react';
@@ -15,7 +15,7 @@ export const ${componentName} = memo((props: ${componentName}Props) => {
     
     return (
         <div className={classNames(cls.${componentName}, {}, [className])}>
-           
+           {t('')}
         </div>
     );
 });`;
