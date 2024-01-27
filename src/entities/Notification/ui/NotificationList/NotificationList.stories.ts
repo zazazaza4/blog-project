7fd81cbce33c6e28949d518fb10b04ef2454d0a9
Yaroslav <6715414@gmail.com>
2024-01-27 18:33:23 +0200

    chore: add stories for NotificationList

---
+++ src/entities/Notification/ui/NotificationList/NotificationList.stories.ts
@@ -0,0 +1,60 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Theme } from '@/app/providers/ThemeProvider';
+
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
+import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+
+import { NotificationList } from './NotificationList';
+
+const meta = {
+  title: 'entities/NotificationButton/NotificationList',
+  component: NotificationList,
+  tags: ['autodocs'],
+  argTypes: {},
+
+} satisfies Meta<typeof NotificationList>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Light: Story = {
+  parameters: {
+    mockData: [
+      {
+        url: `${__API__}/notifications`,
+        method: 'GET',
+        status: 200,
+        response: [
+          { rate: 4 },
+        ],
+      },
+    ],
+  },
+};
+Light.decorators = [
+  StoreDecorator({}),
+];
+
+export const Dark: Story = {
+  parameters: {
+    mockData: [
+      {
+        url: `${__API__}/notifications`,
+        method: 'GET',
+        status: 200,
+        response: [
+          {
+            id: '1',
+            title: 'title',
+            description: 'description',
+          },
+        ],
+      },
+    ],
+  },
+};
+Dark.decorators = [
+  ThemeDecorator(Theme.DARK),
+  StoreDecorator({}),
+];
