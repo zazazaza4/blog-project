246b54e1b72fcf672ca25fd6b4c658f792eec416
Yaroslav <6715414@gmail.com>
2024-01-27 18:33:08 +0200

    feat: enhance webpack config for Storybook

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -10,10 +10,16 @@ export default ({ config }: { config: webpack.Configuration }) => {
     html: '',
     entry: '',
     src: path.resolve(__dirname, '..', '..', 'src'),
+    locales: '',
+    buildLocales: '',
   };
 
   config!.resolve!.modules!.push(paths.src);
   config.resolve?.extensions?.push('.ts', '.tsx');
+  config!.resolve!.alias = {
+    ...config!.resolve!.alias,
+    '@': paths.src,
+  };
 
   // eslint-disable-next-line no-param-reassign
   // @ts-ignore
@@ -32,7 +38,7 @@ export default ({ config }: { config: webpack.Configuration }) => {
 
   config!.plugins!.push(new DefinePlugin({
     __IS_DEV__: JSON.stringify(true),
-    __API__: JSON.stringify(''),
+    __API__: JSON.stringify('https://example.com'),
     __PROJECT__: JSON.stringify('storybook'),
   }));
 
